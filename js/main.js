4fa1a5a06a9b8ab634da4e8e23c57dce577fe37f
bianca.i.mena@gmail.com <>
2014-08-13 22:52:26 +0000

    Flappy Boxes

--- js/main.js
+++ js/main.js
@@ -10,23 +10,96 @@ var mainState = {
   preload: function () {
     //This function will execute at the beginning of the game
     //Here will load all of our assets (art, music, etc)
+    
+    //Stage is where everything happens on the screen
+    
+    game.stage.backgroundColor = "#71c5cf";
+    
+    game.load.image('bird', 'assets/bird.png');
+    
+    game.load.image('pipe', 'assets/pipe.png');
       
   },
   
-  creat: function () {
+  create: function () {
     //The create function is called right after the preload f(X)
     //This is where we'll set up the game assets from  scratch
+    
+    //This starts our physics engine
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    
+    this.bird = this.game.add.sprite(100, 250, 'bird');
+   
+   game.physics.arcade.enable(this.bird);
+    
+    //body: how physics applies to gaming
+    //in gaming, the x and y axis are opposite directions than in a math class
+    this.bird.body.gravity.y = 1000;
+ 
+   //add Pipes to the game 
+   this.pipes = game.add.group();
+   
+   //add the body to the game
+   this.pipes.enableBody = true;
+   
+   //Create 20 pipes to hold in the group
+   this.pipes.createMultiple(20, 'pipe');
+   
+   //Add in pipes over 1.5 seconds to the screen
+   this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+    
+   //When spacebar is pressed, we want to make the bird jump!
+   var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+   spaceKey.onDown.add(this.jump, this);
     
   },
   
   update: function () {
-  //This function is called 60 times a second
-  //
+    //This function is called 60 times a second
+    //It contains the games logic and all time related actions
+    
+    if (this.bird.inWorld == false){
+      this.restartGame();
+    }
+  },
+  
+  addOnePipe: function (x,y){
+    //Get the first dead pipe
+    var pipe = this.pipes.getFirstDead();
+    
+    //Set x and y values of the pipe
+    pipe.reset(x,y);
+    
+    //Move the pipes to the left of the screen
+    pipe.body.velocity.x = -200;
+    
+    //Kill the pipe that is not visible on the screen
+    pipe.checkWorldBounds = true;
+    pipe.outOfBoundsKill = true;
+  },
+  
+  //This game needs a whole number, not zero. 
+  addRowOfPipes: function (){
+    var hole = Math.floor(Math.random()*5) + 1;
+    
+    for (var i = 0; i < 8; i++){
+      if (i != hole && i != hole + 1){
+        this.addOnePipe(400, i*60 + 10);          
+      }
+    }
+  },
+  
+  jump: function (){
+    //Let's add vertical veolicty to the bird whem the spacebar is pressed down
+    
+    this.bird.body.velocity.y = -350;
     
   },
   
+  restartGame: function (){
     
-  
+    game.state.start('main');
+  },
 }
 
 
